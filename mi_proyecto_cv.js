48e8b79893d720ac9d2fc556263d079e6eacff9f
NadiaFigueredo <124043833+NadiaFigueredo@users.noreply.github.com>
2023-01-31 08:20:59 -0300

    Add files via upload
    Proyecto cv del curso Primeros pasos del Desarrollo Frontend.

---
+++ mi_proyecto_cv.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("boton").addEventListener("click", function() {
+    document.getElementById("demo").style.display = "block";
+})
+
+
+
+document.getElementById("boton_ocultar").addEventListener("click", function() {
+    document.getElementById("demo").style.display = "none";
+    } )
+
+    
+    
+
+    
+       
